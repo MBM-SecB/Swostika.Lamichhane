a65c11c313ba7c401ec3e371c1f75ec31e856c97
swostika61 <swostikalamichhane61@gmail.com>
2020-10-16 09:44:25 +0545

    added script

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,29 +1,35 @@
-using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 public class EmployeeController : Controller
 {
     public ActionResult<string> Index()
     {
+        var employees = Employee.GetEmployees();
+
         // List<Employee> employees = new List<Employee>();
-        var employee1 = new Employee()
-        {
-            FirstName = "Random",
-            LastName = "Guy",
-            Address = "NoLand",
-            Salary = 00000
 
-        };
-        var employee2 = new Employee { FirstName = "swostika", LastName = "Lamichhane", Address = "Ktm", Salary = 500 };
-        var employee3 = new Employee { FirstName = "Rabina", LastName = "Karki", Address = "Ktn", Salary = 300 };
-        var employee4 = new Employee { FirstName = "Kritika", LastName = "Lamichhane", Address = "Ktm", Salary = 15000 };
-        var employee5 = new Employee { FirstName = "sam", LastName = "Pal", Address = "Poland", Gender = 'M', Salary = 400 };
-        var employee6 = new Employee { FirstName = "sammy", LastName = "Swift", Address = "Norway", Gender = 'M', Salary = 200 };
-
-        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
 
 
 
 
         return View(employees);
+    }
+
+    public ActionResult Detail(string firstName)
+    {
+        var employees = Employee.GetEmployees();
+        var employee = employees.Where(x => x.FirstName == firstName).First();
+        return View(employee);
+    }
+
+    public ActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult<bool> Add(Employee employee)
+    {
+        return true;
     }
 }
